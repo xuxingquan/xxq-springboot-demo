579ed6b21b8a36d3d3a42d651548d1682430ceea
xuxingquan <461251250@qq.com>
2019-01-23 08:37:34 +0800

    init

---
+++ src/main/java/com/xxq/springboot/BootApplication.java
@@ -0,0 +1,12 @@
+package com.xxq.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+
+
+}

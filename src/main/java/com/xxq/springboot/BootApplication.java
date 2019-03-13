63424352959143cc7aafd1723d8ecd3eafb5c20e
xuxingquan <461251250@qq.com>
2019-03-13 19:12:43 +0800

    test

--- src/main/java/com/xxq/springboot/BootApplication.java
+++ src/main/java/com/xxq/springboot/BootApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 
 public class BootApplication {
 
-
-    //213213
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
     }

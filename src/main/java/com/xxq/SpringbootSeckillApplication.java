2a62b03d4788fc8d4b1f0f13697591cc5bfec157
xuxingquan <461251250@qq.com>
2019-03-19 16:14:03 +0800

    init

---
+++ src/main/java/com/xxq/SpringbootSeckillApplication.java
@@ -0,0 +1,11 @@
+package com.xxq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSeckillApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSeckillApplication.class, args);
+    }
+}

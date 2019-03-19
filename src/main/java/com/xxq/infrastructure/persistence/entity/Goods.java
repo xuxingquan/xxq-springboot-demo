114aba4b7b1239c64c09f3b5ae3ea2c26029d80c
xuxingquan <461251250@qq.com>
2019-03-20 00:33:27 +0800

    mybatis

---
+++ src/main/java/com/xxq/infrastructure/persistence/entity/Goods.java
@@ -0,0 +1,66 @@
+package com.xxq.infrastructure.persistence.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Goods {
+    private Long id;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private Long customerId;
+
+    private Date updateTime;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ src/main/java/com/xxq/infrastructure/persistence/mapper/GoodsMapper.java
@@ -0,0 +1,26 @@
+package com.xxq.infrastructure.persistence.mapper;
+
+import com.xxq.infrastructure.persistence.entity.Goods;
+import com.xxq.infrastructure.persistence.entity.GoodsExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface GoodsMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Goods record);
+
+    int insertSelective(Goods record);
+
+    List<Goods> selectByExample(GoodsExample example);
+
+    Goods selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Goods record, @Param("example") GoodsExample example);
+
+    int updateByExample(@Param("record") Goods record, @Param("example") GoodsExample example);
+
+    int updateByPrimaryKeySelective(Goods record);
+
+    int updateByPrimaryKey(Goods record);
+}

1d1f47336d5fce0224ffc859ebf3a7bfc20ba1be
xuxingquan <461251250@qq.com>
2019-03-19 16:53:38 +0800

    teset

--- src/main/java/com/xxq/SpringbootApplication.java
+++ src/main/java/com/xxq/SpringbootApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootSeckillApplication {
+public class SpringbootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootSeckillApplication.class, args);
+        SpringApplication.run(SpringbootApplication.class, args);
     }
 }
